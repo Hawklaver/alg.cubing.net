ac6aa159bf8648a02b5f9599db5bb73ca84ecc7b
Hawklaver <Hawklaver@users.noreply.github.com>
2024-12-15 20:00:18 +0900

    ページタイトルのフィルター処理で空文字を考慮

--- docs/alg.cubing.net.js
+++ docs/alg.cubing.net.js
@@ -11,11 +11,7 @@ algxApp.config(["$locationProvider", function($locationProvider) {
 
 algxApp.filter("title", function() {
 	return function(input, title) {
-		var prefix = title ? title + " | " : "";
-		if (input.length > 20) {
-			return prefix + input.slice(0, 20) + (input.slice(20, 30) + " ").split(" ")[0] + "...";
-		}
-		return prefix + input;
+		return [title, input].filter(v => v.trim()).join(" | ");
 	};
 });
 
